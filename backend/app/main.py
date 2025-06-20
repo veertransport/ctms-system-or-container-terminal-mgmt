aeb06e9a500c04d08f7e93e8701f2e2e74cee0b2
veertransport <dhaliwalveer123@icloud.com>
2025-06-20 20:25:41 +1200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from app.routes import containers
 
 app = FastAPI()
+
+# Register container routes
+app.include_router(containers.router)
 
 @app.get("/")
 def read_root():
